4a76a656db9bc58b27b16bb36ca3114778cff9b8
EdoLisso99 <lissoni.edoardo99@gmail.com>
2022-01-02 20:15:17 +0100

    :rocket: Added first page of the resumee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstpageComponent } from './components/firstpage/firstpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpageComponent,
   ],
   imports: [
     BrowserModule,
