5f2c1ce052225aac51ccc5883d774886617e8b0d
EdoLisso99 <lissoni.edoardo99@gmail.com>
2022-01-06 15:14:58 +0100

    :construction: Message handling sufficiently settled

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {scroll} from "../../../assets/utilities";
-import {FormControl} from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
@@ -12,9 +13,11 @@ export class ContactComponent implements OnInit, OnChanges {
   share_eng: string = "We'll never share your email with anyone else.";
   share_ita: string = "Non condivideremo mai la tua email con nessun altro.";
 
-  name = new FormControl('');
-  msg = new FormControl('');
-  email = new FormControl('');
+  formGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    message: new FormControl('', [Validators.required, Validators.minLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.email])
+  });
 
   constructor() { }
 
@@ -30,11 +33,51 @@ export class ContactComponent implements OnInit, OnChanges {
     scroll(str);
   }
 
+  fromEngToIta(str: string) : string{
+    if(str === "name"){
+      return "il nome";
+    }
+    else if(str === "message"){
+      return "il messaggio";
+    }
+    else{
+      return "la email";
+    }
+  }
+
   sendEmail(e: Event) {
     e.preventDefault();
-    this.name.reset();
-    this.msg.reset();
-    this.email.reset();
-
+    console.log(this.formGroup);
+    if(this.formGroup.valid){
+      alert(this.flag ? "Il messaggio è stato inviato con successo" : "Your message has been sent successfully");
+    }
+    else {
+      let arr: string[] = [];
+      for (let controlsKey in this.formGroup.controls) {
+        if(!this.formGroup.controls[controlsKey].valid){
+          arr.push(controlsKey.toString());
+        }
+      }
+      if(this.flag){
+        let str = "Errore! ";
+        arr.forEach(err => str += this.fromEngToIta(err) + " ");
+        if(arr.length > 1){
+          alert(str + "sono invalidi!");
+        }
+        else {
+          alert(str + "è invalido!");
+        }
+      }
+      else {
+        let str = "Error! ";
+        arr.forEach(err => str += err + " ");
+        if(arr.length > 1){
+          alert(str + "are invalid!");
+        }
+        else {
+          alert(str + "is invalid!");
+        }
+      }
+    }
   }
 }
